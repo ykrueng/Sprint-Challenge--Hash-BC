3f633a99e14bcb62de43aa881434b8842d72c44c
Yasirah Krueng <yasirah.krueng@arundo.com>
2020-02-07 21:36:04 -0600

    solve ex2

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -16,8 +16,19 @@ def reconstruct_trip(tickets, length):
     hashtable = HashTable(length)
     route = [None] * length
 
-    """
-    YOUR CODE HERE
-    """
-
+    for ticket in tickets:
+        hash_table_insert(hashtable, ticket.source, ticket.destination)
+    
+    sorted = False
+    source = 'NONE'
+    index = 0
+    while not sorted:
+        destination = hash_table_retrieve(hashtable, source)
+        route[index] = destination
+        index += 1
+        if destination != 'NONE':
+            source = destination
+        else:
+            sorted = True
+        print(route)
     return route
