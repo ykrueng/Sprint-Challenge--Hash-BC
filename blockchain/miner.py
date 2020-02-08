2d0bbf7108b5d98e6a98ebf1ac0d22868e467891
Yasirah Krueng <yasirah.krueng@arundo.com>
2020-02-07 20:30:39 -0600

    solve blockchain task and mine several coins

--- blockchain/miner.py
+++ blockchain/miner.py
@@ -7,7 +7,7 @@ from uuid import uuid4
 
 from timeit import default_timer as timer
 
-import random
+from random import randint
 
 
 def proof_of_work(last_proof):
@@ -23,8 +23,13 @@ def proof_of_work(last_proof):
     start = timer()
 
     print("Searching for next proof")
-    proof = 0
-    #  TODO: Your code here
+    proof = last_proof*randint(0, 100)
+    
+    b_last_proof = f'{last_proof}'.encode()
+    last_hash = hashlib.sha256(b_last_proof).hexdigest()
+
+    while valid_proof(last_hash, proof) is False:
+        proof += 1
 
     print("Proof found: " + str(proof) + " in " + str(timer() - start))
     return proof
@@ -39,8 +44,10 @@ def valid_proof(last_hash, proof):
     IE:  last_hash: ...AE9123456, new hash 123456E88...
     """
 
-    # TODO: Your code here!
-    pass
+    b_guess = f'{proof}'.encode()
+    guess_hash = hashlib.sha256(b_guess).hexdigest()
+    return last_hash[-6:] == guess_hash[:6]
+    
 
 
 if __name__ == '__main__':
